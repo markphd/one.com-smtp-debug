b1144abb83d78a3ad0b7613c25492a9397a44152
Mark Philip D <radical.mark@gmail.com>
2017-01-07 08:46:49 +0400

    test msgHTML without contents.html

--- mailer.php
+++ mailer.php
@@ -118,7 +118,7 @@ $mail->addAddress($recipient, 'Receiver');
 $mail->Subject = 'SMTP test via PHP';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+$mail->msgHTML("<h1>One.com</h1><pre>SMTP Mail test: OK</pre>");
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
 
